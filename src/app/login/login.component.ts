27bec523a5a7181e9eea2549ed91a34406cbf00c
Kongpob <kongpob2003@gmail.com>
2025-12-17 11:45:48 +0700

    หน้าhome
    1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,4 +7,11 @@ import { Component } from '@angular/core';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  constructor(private router: Router) {}
+
+  login() {
+    this.router.navigate(['/home']);
+  }
+}
