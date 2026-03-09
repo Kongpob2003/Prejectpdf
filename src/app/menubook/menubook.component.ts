b56ea2b8a3760fa9c1c29b46107d259abad48b5d
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2026-03-09 16:13:26 +0700

    add url คู่มือ

--- src/app/menubook/menubook.component.ts
+++ src/app/menubook/menubook.component.ts
@@ -12,7 +12,7 @@ export class Menubook {
 manuals = [
   {
     name: "คู่มือการใช้งานระบบ.pdf",
-    url: "assets/manuals/manual1.pdf"
+    url: "https://cdn.csmsu.net/extproj_mypdf/adb18786-c983-4fd4-9cea-c56c7914280c.pdf"
   },
 ];
 
@@ -23,7 +23,7 @@ openFile(url: string) {
 downloadFile(url: string) {
   const link = document.createElement('a');
   link.href = url;
-  link.download = '';
+  link.download = 'https://cdn.csmsu.net/extproj_mypdf/adb18786-c983-4fd4-9cea-c56c7914280c.pdf?download=true';
   link.click();
 }
 }
