675cff0f9d484a060bd9ae940fbc9dcdb38408c2
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2026-03-10 13:59:58 +0700

    แก้ menu

--- src/app/menubook/menubook.component.ts
+++ src/app/menubook/menubook.component.ts
@@ -12,7 +12,7 @@ export class Menubook {
 manuals = [
   {
     name: "คู่มือการใช้งานระบบ.pdf",
-    url: "https://cdn.csmsu.net/extproj_mypdf/9b1ec4dd-7fd5-40a4-9ebe-5ec90ecc89cd.pdf"
+    url: "https://cdn.csmsu.net/extproj_mypdf/939eeea0-ced5-4911-81ff-9e7d8c13e1e3.pdf"
   },
 ];
 
@@ -23,7 +23,7 @@ openFile(url: string) {
 downloadFile(url: string) {
   const link = document.createElement('a');
   link.href = url;
-  link.download = 'https://cdn.csmsu.net/extproj_mypdf/9b1ec4dd-7fd5-40a4-9ebe-5ec90ecc89cd.pdf?download=true';
+  link.download = 'https://cdn.csmsu.net/extproj_mypdf/939eeea0-ced5-4911-81ff-9e7d8c13e1e3.pdf?download=true';
   link.click();
 }
 }
