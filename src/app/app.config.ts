adab841a0c6898f5b91ac7337e885b23783be031
Saifa107 <162133763+Saifa107@users.noreply.github.com>
2026-03-11 09:32:41 +0700

    router

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,11 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient , withFetch } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),provideHttpClient(withFetch()),
-    provideRouter(routes), provideClientHydration(withEventReplay()),
-    provideAnimationsAsync()
+    provideRouter(routes), provideClientHydration(withEventReplay()),   
   ]
 };
